0a71359eaac0d0d3987eb317d84ad619ec83e3a5
Jānis Gaiķēns <janis.gaikens@scandiweb.com>
2018-10-12 13:01:15 +0300

    Update widgets.py

--- geoposition/widgets.py
+++ geoposition/widgets.py
@@ -11,7 +11,7 @@ from .conf import settings
 
 
 class GeopositionWidget(forms.MultiWidget):
-    template_name = 'django/forms/widgets/multiwidget.html'
+    template_name = 'geoposition/widgets/geoposition.html'
 
     def __init__(self, attrs=None):
         widgets = (
